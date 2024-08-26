3119b6d0f9dabddb4d1562fff7c89ea74420fb0d
tfaycal <faycal.touazi@gmail.com>
2024-08-27 00:27:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,38 +53,38 @@ def train(rank, world_size, net, data_loader, train_optimizer,temperature):
         num_samples = 0
 
         for batch_idx in range(world_size):
-            # Fetch the split data for this GPU
-            pos_1_batch = pos_1_split[batch_idx]
-            pos_2_batch = pos_2_split[batch_idx]
-            target_batch = target_split[batch_idx]
-
-            # Zero the gradients
-            train_optimizer.zero_grad()
-
-            # Forward pass
-            feature_1, out_1 = net(pos_1_batch)
-            feature_2, out_2 = net(pos_2_batch)
+          # Fetch the split data for this GPU
+          pos_1_batch = pos_1_split[batch_idx]
+          pos_2_batch = pos_2_split[batch_idx]
+          target_batch = target_split[batch_idx]
+          
+          # Zero the gradients
+          train_optimizer.zero_grad()
+          
+          # Forward pass
+          feature_1, out_1 = net(pos_1_batch)
+          feature_2, out_2 = net(pos_2_batch)
              # [2*B, D]
-             out = torch.cat([out_1, out_2], dim=0)
-             # [2*B, 2*B]
-             sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
-         
-             mask = (torch.ones_like(sim_matrix) - torch.eye(2 * pos_1.size(0), device=sim_matrix.device)).bool()
-             # [2*B, 2*B-1]
-             sim_matrix = sim_matrix.masked_select(mask).view(2 * pos_1.size(0), -1)
-     
-             # compute loss
-             pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
-             # [2*B]
-             pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
-             loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
-             train_optimizer.zero_grad(set_to_none=True)
-             loss.backward()
-             loss_accumulator += loss.item() * pos_1_batch.size(0)
-             num_samples += pos_1_batch.size(0)
-
-             # Update weights
-             train_optimizer.step()
+          out = torch.cat([out_1, out_2], dim=0)
+          # [2*B, 2*B]
+          sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+          
+          mask = (torch.ones_like(sim_matrix) - torch.eye(2 * pos_1.size(0), device=sim_matrix.device)).bool()
+          # [2*B, 2*B-1]
+          sim_matrix = sim_matrix.masked_select(mask).view(2 * pos_1.size(0), -1)
+          
+          # compute loss
+          pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+          # [2*B]
+          pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+          loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+          train_optimizer.zero_grad(set_to_none=True)
+          loss.backward()
+          loss_accumulator += loss.item() * pos_1_batch.size(0)
+          num_samples += pos_1_batch.size(0)
+          
+          # Update weights
+          train_optimizer.step()
         # Clip gradients (if needed, optional)
         # for param in net.parameters():
         #     if param.grad is not None:
