c51badcba85b7455f1f8337efcb5aad177e47258
tfaycal <faycal.touazi@gmail.com>
2024-08-26 23:23:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -164,6 +164,7 @@ def main(rank, world_size):
     else:
          print('hello')
 if __name__ == "__main__":
+    world_size = torch.cuda.device_count() if torch.cuda.is_available() else 1    
     mp.spawn(main, args=(world_size,), nprocs=world_size, join=True)
 
 
