60a32e9ce6566893505bfa1971870fe985f75b88
tfaycal <faycal.touazi@gmail.com>
2024-08-26 20:53:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -142,5 +142,5 @@ def main(rank, world_size):
                     torch.save(model.state_dict(), 'results/{}_model.pth'.format(save_name_pre))
 
 if __name__ == '__main__':
-    world_size = torch.cuda.device_count()
+    world_size =1
     mp.spawn(main, args=(world_size,), nprocs=world_size, join=True)
