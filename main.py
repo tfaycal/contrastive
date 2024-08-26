50fd6219360b4f325bfdaea9c3936d49c60c103b
tfaycal <faycal.touazi@gmail.com>
2024-08-26 23:19:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,9 @@ import utils
 from model import Model
 
 def setup_distributed(rank, world_size):
-    temp_dir = '/tmp'  # Or another directory accessible to all processes
-    init_file = os.path.abspath(os.path.join(temp_dir, '.torch_distributed_init'))
-
-    if os.name == 'nt':
-        init_method = 'file:///' + init_file.replace('\\', '/')
-        dist.init_process_group(backend='gloo', init_method=init_method, rank=rank, world_size=world_size)
-    else:
-        init_method = f'file://{init_file}'
-        dist.init_process_group(backend='nccl', init_method=init_method, rank=rank, world_size=world_size)
-
-
+    # Initialize the process group
+    torch.distributed.init_process_group(backend='nccl', rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
 def cleanup():
     dist.destroy_process_group()
 def train(rank, world_size, net, data_loader, train_optimizer):
@@ -104,6 +96,69 @@ def test(rank, world_size, net, memory_data_loader, test_data_loader):
             test_bar.set_description(f'Test Epoch: [{epoch}/{epochs}] Acc@1:{total_top1 / total_num * 100:.2f}% Acc@5:{total_top5 / total_num * 100:.2f}%')
 
     return total_top1 / total_num * 100, total_top5 / total_num * 100
+
+def main(rank, world_size):
+    setup_distributed(rank, world_size)
+    device = torch.device(f"cuda:{rank}" if torch.cuda.is_available() else "cpu")
+
+    if rank == 0:
+        parser = argparse.ArgumentParser(description='Train SimCLR')
+        parser.add_argument('--feature_dim', default=128, type=int, help='Feature dim for latent vector')
+        parser.add_argument('--temperature', default=0.5, type=float, help='Temperature used in softmax')
+        parser.add_argument('--k', default=200, type=int, help='Top k most similar images used to predict the label')
+        parser.add_argument('--batch_size', default=512, type=int, help='Number of images in each mini-batch')
+        parser.add_argument('--epochs', default=500, type=int, help='Number of sweeps over the dataset to train')
+
+        args = parser.parse_args()
+        feature_dim, temperature, k = args.feature_dim, args.temperature, args.k
+        batch_size, epochs = args.batch_size, args.epochs
+
+        # Data preparation
+        train_data = CustomDatasetPair(data=utils.train_images, targets=utils.train_labels, transform=utils.train_transform)
+        memory_data = CustomDatasetPair(data=utils.mem_images, targets=utils.mem_labels, transform=utils.test_transform)
+        test_data = CustomDatasetPair(data=utils.test_images, targets=utils.test_labels, transform=utils.test_transform)
+
+        train_sampler = DistributedSampler(train_data)
+        test_sampler = DistributedSampler(test_data)
+        memory_sampler = DistributedSampler(memory_data)
+
+        train_loader = DataLoader(train_data, batch_size=batch_size, sampler=train_sampler, pin_memory=True)
+        test_loader = DataLoader(test_data, batch_size=batch_size, sampler=test_sampler, pin_memory=True)
+        memory_loader = DataLoader(memory_data, batch_size=batch_size, sampler=memory_sampler, pin_memory=True)
+
+        # Model setup and optimizer config
+        model = Model(feature_dim).to(device)
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[device], output_device=device)
+        
+        flops, params = profile(model.module, inputs=(torch.randn(1, 3, 32, 32).to(device),))
+        flops, params = clever_format([flops, params])
+        if rank == 0:
+            print('# Model Params: {} FLOPs: {}'.format(params, flops))
+        optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-6)
+
+        # Training loop
+        results = {'train_loss': [], 'test_acc@1': [], 'test_acc@5': []}
+        save_name_pre = f'{feature_dim}_{temperature}_{k}_{batch_size}_{epochs}'
+        if rank == 0 and not os.path.exists('results'):
+            os.mkdir('results')
+        best_acc = 0.0
+        for epoch in range(1, epochs + 1):
+            train_loss = train(rank, world_size, model, train_loader, optimizer)
+            results['train_loss'].append(train_loss)
+            test_acc_1, test_acc_5 = test(rank, world_size, model, memory_loader, test_loader)
+            results['test_acc@1'].append(test_acc_1)
+            results['test_acc@5'].append(test_acc_5)
+            if rank == 0:
+                # Save statistics
+                data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
+                data_frame.to_csv(f'results/{save_name_pre}_statistics.csv', index_label='epoch')
+                if test_acc_1 > best_acc:
+                    best_acc = test_acc_1
+                    torch.save(model.state_dict(), f'results/{save_name_pre}_model.pth')
+
+if __name__ == "__main__":
+    # Example call, you would need to pass actual values for rank and world_size
+    main(rank=0, world_size=4)  # Adjust rank and world_size as needed
 
 def main(rank, world_size):
     setup_distributed(rank, world_size)
