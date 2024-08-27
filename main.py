12beb4296f641b5ce50a6d8d95e0823203c0f01d
tfaycal <faycal.touazi@gmail.com>
2024-08-27 11:18:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,34 +14,41 @@ from model import Model
 DEFAULT_TIMEOUT = timedelta(seconds=10)
 
 # Function to train for one epoch
-def train(net, data_loader, train_optimizer, temperature):
-    net.train()
-    total_loss, total_num, train_bar = 0.0, 0, tqdm(data_loader)
-    for pos_1, pos_2, target in train_bar:
-        pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
-        feature_1, out_1 = net(pos_1)
-        feature_2, out_2 = net(pos_2)
+for pos_1, pos_2, target in train_bar:
+    pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
+    
+    # Zero gradients for the optimizer and enable gradient calculation for the model
+    train_optimizer.zero_grad(set_to_none=True)
+    net.requires_grad_(True)
 
-        out = torch.cat([out_1.clone(), out_2.clone()], dim=0)
-        sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+    # Calculate features and output
+    feature_1, out_1 = net(pos_1)
+    feature_2, out_2 = net(pos_2)
 
-        mask = (torch.ones_like(sim_matrix) - torch.eye(2 * pos_1.size(0), device=sim_matrix.device)).bool()
-        sim_matrix = sim_matrix.masked_select(mask).view(2 * pos_1.size(0), -1)
+    # Concatenate outputs and compute similarity matrix
+    out = torch.cat([out_1, out_2], dim=0)
+    sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
 
-        pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
-        pos_sim = torch.cat([pos_sim.clone(), pos_sim.clone()], dim=0)
+    mask = (torch.ones_like(sim_matrix) - torch.eye(2 * pos_1.size(0), device=sim_matrix.device)).bool()
+    sim_matrix = sim_matrix.masked_select(mask).view(2 * pos_1.size(0), -1)
 
-        loss = (-torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+    # Compute loss
+    pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+    pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+    loss = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
 
-        train_optimizer.zero_grad()
-        loss.backward(retain_graph = True)
-        train_optimizer.step()
+    # Accumulate gradients
+    loss.backward()
 
-        total_num += pos_1.size(0)
-        total_loss += loss.item() * pos_1.size(0)
-        train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, epochs, total_loss / total_num))
+    # Step the optimizer
+    train_optimizer.step()
+    net.requires_grad_(False)
 
-    return total_loss / total_num
+    # Update metrics
+    total_num += pos_1.size(0)
+    total_loss += loss.item() * pos_1.size(0)
+    train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, epochs, total_loss / total_num))
+
 
 # Function to test for one epoch
 def test(net, memory_data_loader, test_data_loader):
